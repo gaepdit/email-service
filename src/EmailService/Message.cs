f248ad0bb1a98e3b41f1c807378718cb05196132
Doug Waldron <douglas.waldron@dnr.ga.gov>
2025-11-25 16:53:52 -0500

    Add validation for email addresses

--- src/EmailService/Message.cs
+++ src/EmailService/Message.cs
@@ -1,4 +1,6 @@
-﻿namespace GaEpd.EmailService;
+﻿using System.Net.Mail;
+
+namespace GaEpd.EmailService;
 
 public record Message
 {
@@ -11,11 +13,11 @@ public record Message
     public List<string> CopyRecipients { get; } = [];
 
     public static Message Create(string subject, string recipient, string? textBody, string? htmlBody,
-        string? senderName = null, string? senderEmail = null, IEnumerable<string>? copyRecipients = null) =>
+        string? senderName = null, string? senderEmail = null, ICollection<string>? copyRecipients = null) =>
         Create(subject, [recipient], textBody, htmlBody, senderName, senderEmail, copyRecipients);
 
     public static Message Create(string subject, ICollection<string> recipients, string? textBody, string? htmlBody,
-        string? senderName = null, string? senderEmail = null, IEnumerable<string>? copyRecipients = null)
+        string? senderName = null, string? senderEmail = null, ICollection<string>? copyRecipients = null)
     {
         if (string.IsNullOrWhiteSpace(subject))
             throw new ArgumentException("A subject must be provided.", nameof(subject));
@@ -25,6 +27,15 @@ public record Message
 
         if (recipients.Any(string.IsNullOrEmpty))
             throw new ArgumentException("Recipient cannot be null, empty, or white space.", nameof(recipients));
+
+        if (!recipients.AreAllValid())
+            throw new ArgumentException("Recipient list includes invalid emails.", nameof(recipients));
+
+        if (!senderEmail.IsValid())
+            throw new ArgumentException("Sender email is invalid.", nameof(senderEmail));
+
+        if (copyRecipients != null && recipients.Count > 0 && !copyRecipients.AreAllValid())
+            throw new ArgumentException("CC recipient list includes invalid emails.", nameof(copyRecipients));
 
         if (string.IsNullOrEmpty(htmlBody) && string.IsNullOrEmpty(textBody))
             throw new ArgumentException("Either a plaintext or HTML body must be provided.", nameof(htmlBody));
@@ -42,5 +53,25 @@ public record Message
         if (copyRecipients != null) message.CopyRecipients.AddRange(copyRecipients);
 
         return message;
+    }
+}
+
+internal static class EmailExtensions
+{
+    public static bool AreAllValid(this ICollection<string> addresses) => addresses.All(IsValid);
+
+    public static bool IsValid(this string? address)
+    {
+        if (string.IsNullOrEmpty(address)) return false;
+
+        try
+        {
+            _ = new MailAddress(address);
+            return true;
+        }
+        catch (Exception)
+        {
+            return false;
+        }
     }
 }
